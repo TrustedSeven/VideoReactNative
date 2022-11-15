4405ecdae1d0fa3bb8c6954e70c59634eb817786
TopDragon523 <97777348+TopDragon523@users.noreply.github.com>
2022-11-15 16:10:32 -0500

    add auth stack

---
+++ src/container/login/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Text } from 'react-native'
+
+const LogInScreen = ({ navigation }) => {
+    return (
+        <Text>Login Screen</Text>
+    )
+}
+
+export default LogInScreen;
---
+++ src/container/success/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Text } from 'react-native'
+
+const LogInScreen = ({ navigation }) => {
+    return (
+        <Text>Success Screen</Text>
+    )
+}
+
+export default LogInScreen;
