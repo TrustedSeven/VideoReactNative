66b308c887fb2705b204c2fe7cbd2379dd1882d3
TopDragon523 <97777348+TopDragon523@users.noreply.github.com>
2022-11-15 15:42:46 -0500

    add authstack

---
+++ src/container/signup/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Text } from 'react-native'
+
+const SignUpScreen = ({ navigation }) => {
+    return (
+        <Text>Sign Up screen </Text>
+    )
+}
+
+export default SignUpScreen;
---
+++ src/container/success/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Text } from 'react-native'
+
+const SuccessScreen = ({ navigation }) => {
+    return (
+        <Text>login successfully </Text>
+    )
+}
+
+export default SuccessScreen;
