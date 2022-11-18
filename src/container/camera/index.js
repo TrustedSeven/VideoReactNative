4610e74c1d0c376af88a3b3b93eb5b1f912c6b9b
crazynavi <117359800+TrustedSeven@users.noreply.github.com>
2022-11-18 15:38:25 -0500

    small change

---
+++ src/container/camera/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Alert, Text } from 'react-native'
+import { RNCamera } from 'react-native-camera'
+
+
+
+
+class CameraScreen extends Component {
+    render() {
+      return (
+        <View style={styles.container}>
+          <RNCamera
+            style={{ flex: 1, alignItems: 'center' }}
+            ref={ref => {
+              this.camera = ref
+            }}
+            // androidCameraPermissionOptions={{
+            //     title: 'Permission to use camera',
+            //     message: 'We need your permission to use your camera',
+            //     buttonPositive: 'Ok',
+            //     buttonNegative: 'Cancel',
+            //   }}
+            // androidRecordAudioPermissionOptions={{
+            //     title: 'Permission to use audio recording',
+            //     message: 'We need your permission to use your audio',
+            //     buttonPositive: 'Ok',
+            //     buttonNegative: 'Cancel',
+            //   }}
+            
+            //captureAudio={false}
+          />
+        </View>
+        // <View>
+        //     <Text>sss</Text>
+        // </View>
+      )
+    }
+  }
+
+  
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: 'black'
+    }
+  })
+  
+  export default CameraScreen
--- src/container/login/index.js
+++ src/container/login/index.js
@@ -1,49 +1,97 @@
-import * as React from 'react'
-import { Button, Text, TextInput, View } from 'react-native'
-import { AuthContext } from '../../AuthProvider'
-import styles from './styles'
+import React, { useState } from 'react'
+import { TouchableOpacity, StyleSheet, View } from 'react-native'
+import { Text } from 'react-native-paper'
+import Background from '../../components/Background'
+import Logo from '../../components/Logo'
+import Header from '../../components/Header'
+import Button from '../../components/Button'
+import TextInput from '../../components/TextInput'
+import BackButton from '../../components/BackButton'
+import { theme } from '../../core/theme'
+import { emailValidator } from '../../helpers/emailValidator'
+import { passwordValidator } from '../../helpers/passwordValidator'
 
-const LogInScreen = ({ navigation }) => {
-    const {login} = React.useContext(AuthContext);
+export default function LogInScreen({ navigation }) {
+  const [email, setEmail] = useState({ value: '', error: '' })
+  const [password, setPassword] = useState({ value: '', error: '' })
 
-    const [userName, setUserName] = React.useState('');
-    const [pass, setPass] = React.useState('');
+  const onLoginPressed = () => {
+    const emailError = emailValidator(email.value)
+    const passwordError = passwordValidator(password.value)
+    // if (emailError || passwordError) {
+    //   setEmail({ ...email, error: emailError })
+    //   setPassword({ ...password, error: passwordError })
+    //   return
+    // }
+    // navigation.reset({
+    //   index: 0,
+    //   routes: [{ name: 'Dashboard' }],
+    // })
+    navigation.navigate('Success')
+  }
 
-    return (
-        <View style={styles.container}>
-            <Text >Login Screen</Text>
-            <View style={styles.authInput}>
-                <Text style={styles.authLabel}>username:</Text>
-                <TextInput
-                    style={styles.textInput}
-                    value={userName}
-                    placeholder='username'
-                    onChangeText={(v) => { setUserName(v) }}
-                />
-            </View>
-            <View style={styles.authInput}>
-                <Text style={styles.authLabel}>password:</Text>
-                <TextInput
-                    style={styles.textInput}
-                    value={pass}
-                    secureTextEntry={true}
-                    placeholder='password'
-                    onChangeText={(v) => { setPass(v) }}
-                />
-            </View>
-            <Button
-                title='Login'
-                onPress={() => {
-                    login(userName, pass);
-                }}
-            />
-            <Button
-                color={'red'}
-                title='Signup'
-                onPress={() => navigation.navigate('SignUp')}
-            />
-        </View>
-    )
+  return (
+    <Background>
+      {/* <BackButton goBack={navigation.goBack} /> */}
+      <Logo />
+      <Header>Welcome back.</Header>
+      <TextInput
+        label="Email"
+        returnKeyType="next"
+        value={email.value}
+        onChangeText={(text) => setEmail({ value: text, error: '' })}
+        error={!!email.error}
+        errorText={email.error}
+        autoCapitalize="none"
+        autoCompleteType="email"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+      />
+      <TextInput
+        label="Password"
+        returnKeyType="done"
+        value={password.value}
+        onChangeText={(text) => setPassword({ value: text, error: '' })}
+        error={!!password.error}
+        errorText={password.error}
+        secureTextEntry
+      />
+      <View style={styles.forgotPassword}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('ResetPasswordScreen')}
+        >
+          <Text style={styles.forgot}>Forgot your password?</Text>
+        </TouchableOpacity>
+      </View>
+      <Button mode="contained" onPress={onLoginPressed}>
+        Login
+      </Button>
+      <View style={styles.row}>
+        <Text>Don’t have an account? </Text>
+        <TouchableOpacity onPress={() => navigation.replace('SignUp')}>
+          <Text style={styles.link}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+    </Background>
+  )
 }
 
-export default LogInScreen;
+const styles = StyleSheet.create({
+  forgotPassword: {
+    width: '100%',
+    alignItems: 'flex-end',
+    marginBottom: 24,
+  },
+  row: {
+    flexDirection: 'row',
+    marginTop: 4,
+  },
+  forgot: {
+    fontSize: 13,
+    color: theme.colors.secondary,
+  },
+  link: {
+    fontWeight: 'bold',
+    color: theme.colors.primary,
+  },
+})
